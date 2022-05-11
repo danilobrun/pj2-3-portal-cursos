343e7a1c51ffc42584934ccd434c19a1906f5d64
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-05-11 00:00:18 -0300

    Corta Header e aplica CSS externo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,39 @@
-import { Button } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import Logo from './assets/img/logo-infnet.png'
 
+function Header () {
+  return (
+    <header>
+      <Navbar bg='dark bg-gradient' expand='md' className='main-navbar'>
+        <Container>
+          <Navbar.Brand href='/'>
+             <img src={Logo} alt='Instituto Infnet' width={181} height={50} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='navbar-header' />
+          <Navbar.Collapse id='navbar-header'>
+            <Nav>
+              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link href='/cursos'>Cursos</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar> 
+    </header>
+  )
+}
+
+function HomeView () {
+  return (
+    <>
+      <Header />
+      <p>Conteúdo</p>
+      <p>Rodapé</p>
+    </>
+  )
+}
 function App() {
   return (
-    <div>
-      <button className="btn btn-primary">testeBootstrapCSS</button>
-      <Button variant='success'>Teste</Button>
-    </div>
+   <HomeView />
   );
 }
 
