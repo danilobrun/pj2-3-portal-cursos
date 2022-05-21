47e1e0595592676837fb148652d18257aee3c024
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-05-21 19:05:05 -0300

    Exibe mensagem de erro na requisição caso a API falhe

--- front-end/src/views/Home/Portals/index.js
+++ front-end/src/views/Home/Portals/index.js
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Alert, Col, Container, Row } from "react-bootstrap";
 import { Layout } from "../../../components/Layout";
 import { CardPortals } from "../../../components/CardPortals";
 import { useEffect, useState } from "react";
@@ -7,11 +7,17 @@ import { Loading } from "../../../components/Loading";
 export function PortalsView () {
     const [portals, setPortals] = useState([])
     const [loading, setLoading] = useState(true)
+    const [errorMsg, setErrorMsg] = useState()
     useEffect(() => {
         fetch('http://localhost:3001/portals')
             .then((response) => response.json())
             .then((data) => {
                 setPortals(data)
+            })
+            .catch(() => {
+                setErrorMsg('Falha ao buscar portais. Recarregue a página. Caso o erro persista entre em contato com administrador Danilo Brun.')
+            })
+            .finally(() => {
                 setLoading(false)
             })
             
@@ -22,6 +28,9 @@ export function PortalsView () {
                 <h1 className='text-center mt-4'>Portais</h1>
                 {loading && (
                     <Loading />
+                )}
+                {errorMsg && (
+                    <Alert variant="danger">{errorMsg}</Alert>
                 )}
                 <Row>
                    {portals.map(portal => (
