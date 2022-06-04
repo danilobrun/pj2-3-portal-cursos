5f2311da263b1ab85bdcfebfbeab3e06f0d05e47
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-06-03 21:49:29 -0300

    Refatora - move reducer para arquivo próprio e refatora user.reducer para swith case

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,19 +1,7 @@
 import { createStore } from 'redux'
-import { getStorageItem } from '../services/Storage.service';
+import { userReducer } from './User/User.reducer'
 
-const user = JSON.parse(getStorageItem('user'))
-
-const reducer = (state, action) => {
-    console.log(state, action)
-    if (action.type === '@@INIT') {
-        return user || null
-    }
-    if (action.type === 'USER_LOGIN') {
-        return action.payload
-    }
-    if (action.type === 'USER_LOGOUT') {
-        return null
-    }
-    return state
-}
-export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export const store = createStore(
+    userReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
